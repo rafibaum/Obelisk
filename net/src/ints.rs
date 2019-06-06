3350dd4b3abee6f0ed53d9a9e46c4035a8ef463e
Rafi Baum <rafi@ukbaums.com>
2019-06-06 12:00:44 +0100

    MAcros work

--- net/src/ints.rs
+++ net/src/ints.rs
@@ -5,38 +5,39 @@ use std::marker::Sized;
 use std::mem::transmute;
 use std::convert::TryInto;
 
-macro_rules! int_codec {
-    ($Type:ident) => {
-        const SIZE_$Type: usize = std::mem::size_of::<$Type>();
+macro_rules! int_base {
+    ($Type:ident, $size:expr) => {
         impl PacketCodec for $Type {
             fn to_mc_bytes(&self) -> Vec<u8> {
                 unsafe {
-                    let bytes: [u8; SIZE_$Type] = transmute(self.clone().to_be());
+                    let bytes: [u8; $size] = transmute(self.clone().to_be());
                     bytes.to_vec()
                 }
             }
 
             fn from_mc_bytes(bytes: &mut Vec<u8>) -> Result<Self, Error> {
-                let bytes: [u8; SIZE_$Type] = bytes.drain(0..SIZE_$Type).try_into()?;
-                Ok($Type::from_be_bytes(bytes))
+                if (bytes.len() < $size) {
+                    return Err(Error::new(ErrorKind::InvalidData, "Byte buffer too short to parse data"));
+                }
+
+                let mut num_bytes = [0u8; $size];
+                let mut index = 0;
+                for byte in bytes.drain(0..$size) {
+                    num_bytes[index] = byte;
+                    index += 1;
+                }
+
+                Ok($Type::from_be_bytes(num_bytes))
             }
         }
     }
 }
 
-//int_codec!(u16);
-const SIZE_u16: usize = std::mem::size_of::<u16>();
-impl PacketCodec for u16 {
-
-    fn to_mc_bytes(&self) -> Vec<u8> {
-        unsafe {
-            let bytes: [u8; SIZE_u16] = transmute(self.clone().to_be());
-            bytes.to_vec()
-        }
-    }
-
-    fn from_mc_bytes(bytes: &mut Vec<u8>) -> Result<Self, Error> {
-        let bytes: [u8; SIZE_u16] = bytes.drain(0..SIZE_u16).try_into()?;
-        Ok(u16::from_be_bytes(bytes))
+macro_rules! int_codec {
+    ($Type:ident) => {
+        int_base!($Type, std::mem::size_of::<$Type>());
     }
 }
+
+int_codec!(u8);
+int_codec!(u8);
